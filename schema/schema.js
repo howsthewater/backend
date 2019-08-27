fa2aa272e4da2316a8b899cb4b690993e4979361
stackblock <stackchain@gmail.com>
2019-08-26 20:19:31 -0400

    api 1

--- schema/schema.js
+++ schema/schema.js
@@ -2,6 +2,7 @@ const graphql = require("graphql");
 // const _ = require("lodash");
 const User = require("../models/user.js");
 const Location = require("../models/location.js");
+const fetch = require("node-fetch");
 const {
   getGraphQLQueryArgs,
   getMongoDbQueryResolver,
@@ -60,11 +61,26 @@ const LocationType = new GraphQLObjectType({
     LONGITUDE: { type: new GraphQLNonNull(GraphQLFloat) },
     Photo_1: { type: new GraphQLNonNull(GraphQLString) },
     Photo_2: { type: new GraphQLNonNull(GraphQLString) },
-    Photo_3: { type: new GraphQLNonNull(GraphQLString) },
+    Photo_3: { type: new GraphQLNonNull(GraphQLString) },  
     Photo_4: { type: new GraphQLNonNull(GraphQLString) },
     Bch_whlchr: { type: new GraphQLNonNull(GraphQLString) },
     BIKE_PATH: { type: new GraphQLNonNull(GraphQLString) },
-    BT_FACIL_TYPE: { type: new GraphQLNonNull(GraphQLString) }
+    BT_FACIL_TYPE: { type: new GraphQLNonNull(GraphQLString) },
+    person: {
+      type: PersonType,
+      resolve (parent, args){
+//        return fetch(`https://swapi.co/api/people/${parent.ID}/`)
+         return fetch(
+           `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=747d8c5349384e6497b03330191808&q= ${parent.LATITUDE}, ${parent.LONGITUDE}&format=json&num_of_days=1&fx=yes&moonrise=yes&tp=24`
+         )
+          .then(response => {
+            console.log(response);
+            return response.json();
+          })
+          .catch(error => console.log(error));
+        
+      }
+    }
     // author: {
     //   type: AuthorType,
     //   resolve(parent, args) {
@@ -75,13 +91,75 @@ const LocationType = new GraphQLObjectType({
   })
 });
 
+const PersonType = new GraphQLObjectType({
+  name: "Person",
+  fields: () => ({
+    data: { type: DataType }
+  })
+});
+
+const DataType = new GraphQLObjectType({
+  name: "Data",
+  fields: () => ({
+    request: { type: new GraphQLList(RequestType) },
+    current_condition: { type: new GraphQLList(Current_ConditionType) },
+    weather: { type: new GraphQLList(WeatherType) }
+  })
+});
+
+const RequestType = new GraphQLObjectType({
+  name: "Request",
+  fields: () => ({
+    type: { type: GraphQLString },
+    query: { type: GraphQLString }
+  })
+});
+
+const Current_ConditionType = new GraphQLObjectType({
+  name: "Current_Condition",
+  fields: () => ({
+    observation_time: { type: GraphQLString },
+    temp_C: { type: GraphQLString  },
+    temp_F: { type: GraphQLString }
+  })
+});
+
+
+const WeatherType = new GraphQLObjectType({
+  name: "Weather",
+  fields: () => ({
+    date: { type: GraphQLString },
+    astronomy: { type: new GraphQLList(AstronomyType) },
+    hourly: { type: new GraphQLList(HourlyType) }
+  })
+});
+
+const AstronomyType = new GraphQLObjectType({
+  name: "Astronomy",
+  fields: () => ({
+    sunrise: { type: GraphQLString },
+    sunset: { type: GraphQLString }
+  })
+});
+
+const HourlyType = new GraphQLObjectType({
+  name: "hourly",
+  fields: () => ({
+    windspeedMiles: { type: GraphQLString },
+    windspeedKmph: { type: GraphQLString },
+    WindChillF: { type: GraphQLString },
+    WindChillC: { type: GraphQLString }
+  })
+});
+
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: () => ({
     id: { type: GraphQLID },
     beachId: { type: new GraphQLNonNull(GraphQLString) },
     name: { type: new GraphQLNonNull(GraphQLString) },
-    age: { type: GraphQLInt },
+    age: { type: GraphQLInt }
     //    bookId: { type: new GraphQLNonNull(GraphQLString) },
     // locations: {
     //   type: new GraphQLList(locationType),
@@ -98,7 +176,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     location: {
       type: LocationType,
-      args: { ID: { type: GraphQLID }},
+      args: { ID: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db or source
         //        return _.find(books, { id: args.id });
@@ -158,7 +236,7 @@ const Mutation = new GraphQLObjectType({
         });
         return user.save();
       }
-    },
+    }
     // addBook: {
     //   type: BookType,
     //   args: {
